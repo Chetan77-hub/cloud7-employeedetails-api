17dc286eec79e55515804f96b02032411a4c85b9
Chetan77-hub <56333502+Chetan77-hub@users.noreply.github.com>
2025-07-13 22:44:51 +0530

    Update UserRepository.java

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.example.demo.repository;
 
 import com.example.demo.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends MongoRepository<User, String>  {
+public interface UserRepository extends JpaRepository<User, Integer>  {
 }
 
 
