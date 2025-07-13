eec6bd1f2eaebc6419d2a3fef4335b990365b8fb
Chetan77-hub <56333502+Chetan77-hub@users.noreply.github.com>
2025-07-13 21:21:57 +0530

    Update User.java

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -24,4 +24,28 @@ public class User {
     }
 
     // Getters and Setters
+     public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
